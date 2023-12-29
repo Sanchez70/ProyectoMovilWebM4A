b948d38d1fba1e05e34b72794b5c6c71b102db7b
Jean15M <127127163+Jean15M@users.noreply.github.com>
2023-12-29 11:40:34 -0500

    PANTALLA RESERVAS
    Se realizó el prototipo de la pantalla de reservar.

---
+++ app/src/main/java/com/ista/zhotel/PantallaReservar.java
@@ -0,0 +1,14 @@
+package com.ista.zhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaReservar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_reservar);
+    }
+}
