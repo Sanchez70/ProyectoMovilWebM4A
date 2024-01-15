fa7a9d69ae5ad6b45ef9e527c2c98af136e570b2
Sanchez70 <124312881+Sanchez70@users.noreply.github.com>
2024-01-15 17:08:54 -0500

    CorreccionLogin

--- app/src/main/java/com/ista/zhotel/PantallaPrincipal.java
+++ app/src/main/java/com/ista/zhotel/PantallaPrincipal.java
@@ -7,15 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class PantallaPrincipal extends AppCompatActivity {
     Spinner comboCate;
+    FirebaseAuth auth;
+    Button inicio;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
+        inicio= findViewById(R.id.button4);
+                inicio.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent= new Intent(getApplicationContext(), Login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
 
 
         comboCate=(Spinner) findViewById(R.id.spinner);
