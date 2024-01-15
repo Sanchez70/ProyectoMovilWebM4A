a9c5e90a32b55bdaa8a83f74643c4c3ab9c82f6e
Sanchez70 <124312881+Sanchez70@users.noreply.github.com>
2024-01-15 09:37:01 -0500

    LoginRegitrarPrueba

--- app/src/main/java/com/ista/zhotel/Login.java
+++ app/src/main/java/com/ista/zhotel/Login.java
@@ -1,14 +1,89 @@
 package com.ista.zhotel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    EditText emailTextLogin, paswordTextLogin;
+    Button botonLogin;
+    TextView textView2;
+    private FirebaseAuth mAuth;
 
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailTextLogin= findViewById(R.id.paswordLogin);
+        paswordTextLogin=findViewById(R.id.paswordLogin);
+        mAuth = FirebaseAuth.getInstance();
+        botonLogin= findViewById(R.id.buttonLogin);
+        textView2 = findViewById(R.id.textViewLo);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),Registrar.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailUser= emailTextLogin.getText().toString().trim();
+                String passwordlUser= paswordTextLogin.getText().toString().trim();
+                if (TextUtils.isEmpty(emailUser)){
+                    Toast.makeText(Login.this, "Enten email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordlUser)){
+                    Toast.makeText(Login.this, "Enter pasword", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                mAuth.signInWithEmailAndPassword(emailUser, passwordlUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Ingreso Exitoso", Toast.LENGTH_SHORT).show();
+                            Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {;
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            ;
+                        }
+                    }
+                });
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/ista/zhotel/Registrar.java
@@ -0,0 +1,87 @@
+package com.ista.zhotel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Registrar extends AppCompatActivity {
+    TextInputEditText emailText, paswordText;
+    Button boton1;
+    TextView textView1;
+    private FirebaseAuth mAuth;
+    public void onStart () {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+        emailText= findViewById(R.id.email);
+        paswordText=findViewById(R.id.pasword);
+        mAuth = FirebaseAuth.getInstance();
+        boton1= findViewById(R.id.button);
+        textView1 = findViewById(R.id.textViewRe);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailUser= emailText.getText().toString().trim();
+                String passwordlUser= paswordText.getText().toString().trim();
+                if (TextUtils.isEmpty(emailUser)){
+                    Toast.makeText(Registrar.this, "Enten email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordlUser)){
+                    Toast.makeText(Registrar.this, "Enter pasword", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                mAuth.createUserWithEmailAndPassword(emailUser, passwordlUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Registrar.this, "Authentication Exit.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent= new Intent(getApplicationContext(),Login.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(Registrar.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            ;
+                        }
+                    }
+                });
+
+            }
+        });
+
+    }
+
+}
