3eb3caa6685981353036fc32e46493c07a978199
Jean15M <127127163+Jean15M@users.noreply.github.com>
2024-01-16 08:39:02 -0500

    Pantalla Reservar 10%

--- app/src/main/java/com/ista/zhotel/PantallaReservar.java
+++ app/src/main/java/com/ista/zhotel/PantallaReservar.java
@@ -3,12 +3,71 @@ package com.ista.zhotel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class PantallaReservar extends AppCompatActivity {
-
+    private TextView txtFechaIn;
+    private TextView txtFechaFin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_reservar);
+        calendar();
+    }
+
+    public void calendar(){
+
+
+        txtFechaIn = findViewById(R.id.fechaInic);
+        txtFechaFin = findViewById(R.id.fechafin);
+
+        txtFechaIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+        txtFechaFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+    }
+
+    private void showDatePicker() {
+        CalendarConstraints.Builder constraintsBuilder = new CalendarConstraints.Builder();
+        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Seleccione la fecha");
+        builder.setCalendarConstraints(constraintsBuilder.build());
+
+        MaterialDatePicker<Long> datePicker = builder.build();
+
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                // Manejar la fecha seleccionada
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(selection);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                String selectedDate = dateFormat.format(calendar.getTime());
+
+                txtFechaIn.setText(selectedDate);
+                txtFechaFin.setText(selectedDate);
+            }
+        });
+
+        datePicker.show(getSupportFragmentManager(), "DATE_PICKER_TAG");
     }
 }
